970e15fa22ab85b6247234d2be0e4787651da419
arjunbector <arjunbector@gmail.com>
2024-10-14 00:27:04 +0530

    fix: handle undefined users in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,8 @@ import { MetadataRoute } from 'next'
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const { baseUrl } = checkEnvironment();
     const usersRes = await fetch(`${baseUrl}/api/users`);
-    const users = await usersRes.json();
+    const { users } = await usersRes.json();
+    if (!users) return [];
     const userEntries: MetadataRoute.Sitemap = users.map((user: { username: string }) => ({
         url: `${baseUrl}/users/${user.username}`,
     }))
